4f6757d65c83b733236bc3cf99d250e6271a1a69
Thomas Delplace <thomasrenedelplace@gmail.com>
2026-03-06 01:27:34 +0100

    fixing header and setting up footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,26 +9,29 @@ export function Footer() {
     <footer className="bg-[#0B3D91] text-white footer-section">
       <img src='/paperBottom.png' style={{position:'absolute', top:'-2px', transform:'scaleY(-1)'}} alt="" />
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
           {/* About Column */}
           {/* <div>
             <h3 className="footer-title">Faisons mieux avec Vous !</h3>
             <p className="text-sm text-blue-200">
-              Programme bientôt disponible ici.
+              La Voix des Oublié·es et de l'Urgence Sociale
+            </p>
+            <p className="text-sm text-blue-200">
+              Élections municipales à Saint-Pierre-des-Corps les 15 et 22 mars 2026
             </p>
           </div> */}
 
           {/* Quick Links */}
-          {/* <div>
+          <div>
             <h4 className="footer-title">Accès rapide</h4>
             <ul className="space-y-2 text-sm">
               <li><Link to="/" className="<footer-link>">Accueil</Link></li>
               <li><Link to="/programme" className="footer-link">Programme</Link></li>
               <li><Link to="/liste" className="footer-link">Liste</Link></li>
               <li><Link to="/evenements" className="footer-link">Événements</Link></li>
-              <li><Link to="" className="footer-link">Événements</Link></li>
+              {/* <li><Link to="" className="footer-link">Événements</Link></li> */}
             </ul>
-          </div> */}
+          </div>
 
           {/* Get Involved */}
           {/* <div>
@@ -40,16 +43,14 @@ export function Footer() {
           </div> */}
 
           {/* Social & Legal */}
-          {/* <div>
+          <div>
             <h4 className="footer-title">Réseaux sociaux</h4>
             <SocialBar id='02'/>
-            <ul className="space-y-2 text-sm">
-            </ul>
-          </div> */}
+          </div>
         </div>
 
         <div className="mt-8 pt-8 text-sm text-center text-blue-200" style={{paddingBottom:'100px'}}>
-          <h4 className='text-white text-3xl lg:text-6xl'>Retrouvez très bientôt notre programme ici ...</h4>
+          {/* <h4 className='text-white text-3xl lg:text-6xl'>Retrouvez très bientôt notre programme ici ...</h4> */}
           <p className="mt-4" style={{color:'var(--color-yellow-400)'}}>&copy; {currentYear} Faisons mieux avec Vous ! Tous droits réservés.</p>
         </div>
       </div>
